fcbcbf8e6500733f6630d9e2d4efec971754b538
Emanuel Inacio <emanuelinacio1@hotmail.com>
2018-10-27 12:05:07 -0700

    Create timer1.h

---
+++ timer1.h
@@ -0,0 +1,15 @@
+// Author:         
+// Net ID:         
+// Date:           
+// Assignment:     Lab 4
+//----------------------------------------------------------------------//
+
+#ifndef TIMER1_H
+#define TIMER1_H
+
+#include <avr/io.h>
+
+void initTimer1();
+void delayUs(unsigned int delay);
+
+#endif
