7bc74d1adb835632300abc9bd69d8e961b6a11b8
EdmundSheah <44382870+EdmundSheah@users.noreply.github.com>
2018-10-29 14:11:56 -0700

    Create ADC.cpp

---
+++ ADC.cpp
@@ -0,0 +1,17 @@
+// Author:        
+// Net ID:        
+// Date:         
+// Assignment:     Lab 4
+//File: ADC.cpp
+//----------------------------------------------------------------------//
+
+#include "ADC.h"
+
+int AnalogRead(int AnalogPin){
+    return 0;
+}
+void AnalogWrite(int AnalogPin, int Val){}
+
+void AnalogWriteResoltion(int bits){}
+
+void AnalogReference(unsigned char Type){}
